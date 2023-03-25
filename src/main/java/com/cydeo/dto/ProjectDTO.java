a69268d7b1804a02bbcf9a2f83cf8454c006a7a6
HadiHarmandar <hadiharmandar1@gmail.com>
2023-03-25 11:27:59 -0500

    Ticketing Part-5

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -15,7 +15,7 @@ public class ProjectDTO {
 
     private String projectName;
     private String projectCode;
-    private UserDTO assignedManaged;
+    private UserDTO assignedManager;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -23,4 +23,16 @@ public class ProjectDTO {
     private String projectDetail;
     private Status projectStatus;
 
+    private int completeTaskCounts;
+    private int unfinishedTaskCounts;
+
+    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
+        this.projectName = projectName;
+        this.projectCode = projectCode;
+        this.assignedManager = assignedManager;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.projectDetail = projectDetail;
+        this.projectStatus = projectStatus;
+    }
 }
